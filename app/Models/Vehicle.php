00519576a7e01fae0c219c3e3789b2db5e2d52c4
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2024-02-02 18:05:29 -0500

    custom image attribute for vehicles

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -25,4 +25,15 @@ class Vehicle extends Model
             fn($value, $attributes) => number_format($attributes['price'] / 100, 2)
         );
     }
+
+    /**
+     * Get the vehicle's custom image.
+    */
+    public function customImage(): Attribute {
+        return Attribute::get(function(): string {
+            return $this->image
+                    ? asset('/storage/images/'.$this->attributes['image'])
+                    : asset('/storage/images/defaults/no_image.jpg');
+        });
+    }
 }
