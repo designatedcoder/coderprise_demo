28110535fc4c4df73c6e6521a70169009d4b76b0
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2024-03-03 19:58:21 -0500

    refactored vehicle routes to a resource

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/vehicles', [VehicleController::class, 'index'])->name('vehicles.index');
-    Route::get('/vehicles/create', [VehicleController::class, 'create'])->name('vehicles.create');
-    Route::post('/vehicles', [VehicleController::class, 'store'])->name('vehicles.store');
-    Route::get('/vehicles/{vehicle:uuid}', [VehicleController::class, 'show'])->name('vehicles.show');
-    Route::get('/vehicles/{vehicle:uuid}/edit', [VehicleController::class, 'edit'])->name('vehicles.edit');
-    Route::patch('/vehicles/{vehicle:uuid}', [VehicleController::class, 'update'])->name('vehicles.update');
-    Route::delete('/vehicles/{vehicle:uuid}', [VehicleController::class, 'destroy'])->name('vehicles.destroy');
+Route::middleware(['auth', 'verified', 'password.confirm'])->group(function () {
+    // Route::get('/vehicles', [VehicleController::class, 'index'])->name('vehicles.index');
+    // Route::get('/vehicles/create', [VehicleController::class, 'create'])->name('vehicles.create');
+    // Route::post('/vehicles', [VehicleController::class, 'store'])->name('vehicles.store');
+    // Route::get('/vehicles/{vehicle:uuid}', [VehicleController::class, 'show'])->name('vehicles.show');
+    // Route::get('/vehicles/{vehicle:uuid}/edit', [VehicleController::class, 'edit'])->name('vehicles.edit');
+    // Route::patch('/vehicles/{vehicle:uuid}', [VehicleController::class, 'update'])->name('vehicles.update');
+    // Route::delete('/vehicles/{vehicle:uuid}', [VehicleController::class, 'destroy'])->name('vehicles.destroy');
+    Route::resource('vehicles', VehicleController::class)->parameters([
+        'vehicles' => 'vehicle:uuid'
+    ]);
 });
 
 
